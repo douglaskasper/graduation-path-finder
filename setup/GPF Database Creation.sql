6931c38685494bb47ecd4779547c12f9c247260c
Douglas Kasper <dkasper879@gmail.com>
2016-11-12 10:57:07 -0600

    Completed db creation, began all initialization, including examples.

--- setup/GPF Database Creation.sql
+++ setup/GPF Database Creation.sql
@@ -1,12 +1,25 @@
+/**
+DROP TABLE tblClassHistory
+DROP TABLE tblGPFSession
+DROP TABLE tblClassOffering
+DROP TABLE tblAccount
+DROP TABLE tblConcentrationCourse
+DROP TABLE tblDegreeRequirement
+DROP TABLE tblCoursePrerequisite
+DROP TABLE tblCourse
+DROP TABLE tblConcentration
+DROP TABLE tblDegree
+*/
+
 IF NOT EXISTS (	SELECT * FROM INFORMATION_SCHEMA.TABLES 
 				WHERE TABLE_SCHEMA = 'dbo' 
 					AND  TABLE_NAME = 'tblDegree')
 BEGIN
 	CREATE TABLE tblDegree (
 		deg_id INT IDENTITY(1,1) NOT NULL,
-		deg_name VARCHAR(20),
-		deg_title VARCHAR(20),
-		deg_description VARCHAR(1000),
+		deg_name VARCHAR(50),
+		deg_title VARCHAR(200),
+		deg_description VARCHAR(max),
 		PRIMARY KEY (deg_id)
 	)
 END
@@ -17,9 +30,9 @@ IF NOT EXISTS (	SELECT * FROM INFORMATION_SCHEMA.TABLES
 BEGIN
 	CREATE TABLE tblConcentration (
 		con_id INT IDENTITY(1,1) NOT NULL,
-		con_title VARCHAR(20),
+		con_title VARCHAR(50),
 		deg_id INT NOT NULL,
-		PRIMARY KEY (con_id)
+		PRIMARY KEY (con_id),
 		FOREIGN KEY (deg_id) REFERENCES tblDegree(deg_id),
 	)
 END
@@ -33,7 +46,7 @@ BEGIN
 		crs_department_code CHAR(3) NOT NULL,
 		crs_number INT NOT NULL,
 		crs_title VARCHAR(50),
-		crs_description VARCHAR(1000),
+		crs_description VARCHAR(max),
 		crs_units DECIMAL(3,2) DEFAULT '4.00',
 		crs_active CHAR(1) DEFAULT 'Y', -- Y = Active, N = Inactive
 		PRIMARY KEY (crs_id)
@@ -68,9 +81,9 @@ END
 
 IF NOT EXISTS (	SELECT * FROM INFORMATION_SCHEMA.TABLES 
 				WHERE TABLE_SCHEMA = 'dbo' 
-					AND  TABLE_NAME = 'tblConcentrationRequirement')
+					AND  TABLE_NAME = 'tblConcentrationCourse')
 BEGIN
-	CREATE TABLE tblConcentrationRequirement (
+	CREATE TABLE tblConcentrationCourse (
 		con_id INT NOT NULL,
 		crs_id INT NOT NULL
 		PRIMARY KEY (con_id, crs_id),
@@ -85,16 +98,16 @@ IF NOT EXISTS (	SELECT * FROM INFORMATION_SCHEMA.TABLES
 BEGIN
 	CREATE TABLE tblAccount (
 		acc_id INT IDENTITY(1,1) NOT NULL,
-		acc_username VARCHAR(50) NOT NULL,
-		acc_password CHAR(128) NOT NULL,
+		acc_username VARCHAR(100) NOT NULL,
+		acc_password VARCHAR(100) NOT NULL,
 		acc_first_name VARCHAR(50) NOT NULL,
 		acc_last_name VARCHAR(50) NOT NULL,
-		acc_street VARCHAR(100),
+		acc_street VARCHAR(200),
 		acc_city VARCHAR(50),
 		acc_state CHAR(2),
 		acc_zip CHAR(15),
 		acc_phone CHAR(10),
-		acc_role VARCHAR(10) NOT NULL DEFAULT 'S', -- A = Admin, F = Faculty, S = Student
+		acc_role VARCHAR(1) NOT NULL DEFAULT 'S', -- A = Admin, F = Faculty, S = Student
 		deg_id INT,
 		con_id INT,
 		PRIMARY KEY (acc_id),
@@ -108,19 +121,17 @@ IF NOT EXISTS (	SELECT * FROM INFORMATION_SCHEMA.TABLES
 					AND  TABLE_NAME = 'tblClassOffering')
 BEGIN
 	CREATE TABLE tblClassOffering (
+		crs_id INT NOT NULL,
 		clo_id INT IDENTITY(1,1) NOT NULL,
+		clo_year INT NOT NULL,
+		clo_quarter CHAR(20) NOT NULL, -- FALL, WINTER, SPRING, SUMMERI, SUMMERII, SUMMER10WEEK, DECINTERSESSION
+		clo_day_of_week CHAR(5) NOT NULL, -- O = Online, M = Monday, TU = Tuesday, W = Wednesday, TH = Thursday, F = Friday, MW = Mon/Wed, MWF = Mon/Wed/Fri, TUTH = Tues/Thurs
+		clo_location CHAR(15) DEFAULT 'ONLINE', -- ONLINE, LOOP, LINCOLNPARK, etc.
 		clo_section_number CHAR(5),
 		clo_status VARCHAR(1) DEFAULT 'O', -- O = Open, W = Wait List, C = Canceled.
-		clo_year INT NOT NULL,
-		clo_quarter CHAR(20), -- Fall, Winter, Spring, SummerI, SummerII, Summer10Week, DecIntersession
-		clo_day_of_week CHAR(5),
-		clo_location CHAR(15) DEFAULT 'ONLINE', -- ONLINE, LOOP, LINCOLNPARK, etc.
-		crs_id INT NOT NULL,
-		-- clr_id INT NULL, -- Classroom ID, NULL = Online.
-		acc_id_instructor INT NOT NULL,
+		acc_id_instructor INT,
 		PRIMARY KEY (clo_id),
 		FOREIGN KEY (crs_id) REFERENCES tblCourse(crs_id),
-		FOREIGN KEY (clr_id) REFERENCES tblClassroom(clr_id),
 		FOREIGN KEY (acc_id_instructor) REFERENCES tblAccount(acc_id)
 	)
 END
@@ -137,7 +148,7 @@ BEGIN
 		gpf_entering_year INT,
 		gpf_entering_quarter CHAR(20),
 		gpf_classes_per_quarter INT,
-		gpf_class_delivery CHAR(2)  DEFAULT 'O', -- C = In Class, O = Online, CO = In Class And Online
+		gpf_class_delivery CHAR(2) DEFAULT 'O', -- C = In Class, O = Online, CO = In Class And Online
 		PRIMARY KEY (gpf_id),
 		FOREIGN KEY (acc_id) REFERENCES tblAccount(acc_id),
 		FOREIGN KEY (deg_id) REFERENCES tblDegree(deg_id),
--- setup/GPF Database Initialization.sql
+++ setup/GPF Database Initialization.sql
@@ -1,54 +1,199 @@
-INSERT INTO tblDegree (
-		deg_name,
-		deg_title,
-		deg_description
-)
-SELECT
-	'Computer Science',
-	'MS In Computer Science',
-	'DePaul''s graduate Computer Science program is the premier training ground for computing professionals in the Chicago area. Unlike a certificate program, our master''s courses focus on fundamental concepts that remain constant, even as technology changes rapidly. Unlike more research focused institutions, however, our courses emphasize the programmatic application of these fundamental concepts. Students interested in research will find an abundance of advanced topics --- medical imaging, data mining, algorithmic analysis, and scientific computing, to name a few.'
+/**
+	DEGREES
 
-INSERT INTO tblDegree (
-		deg_name,
-		deg_title,
-		deg_description
-)
-SELECT
-	'Computer Science',
-	'MS In Computer Science',
-	'The MS in Information Systems fits your career path if it involves running an IT organization or managing information effectively through information systems. In today''s environment, information systems (IS) are planned, operated and maintained not only by IS departments but also by IS-user organizational units. Because of this reality, career opportunities for IS graduates are found in both traditional IS departments and IS-user business units. Moreover, a variety of equipment from aircraft to household electronics frequently use embedded systems. Graduates of IS majors find opportunities in organizations that plan, design and market the functionality of these systems. Thus, our MS-IS emphasizes the balance between technical skills, modeling proficiency, as well as strategic and organizational knowledge for a wide range of IS usage contexts.'
+	select * from tbldegree
+*/
 
-INSERT INTO tblConcentration (
-		con_title,
-		deg_id
-)
-SELECT
-	'Software and Systems Development',
-	deg.deg_id
+INSERT INTO tblDegree (deg_name, deg_title, deg_description)
+SELECT	'Computer Science',
+		'MS In Computer Science',
+		'DePaul''s graduate Computer Science program is the premier training ground for computing professionals in the Chicago area. Unlike a certificate program, our master''s courses focus on fundamental concepts that remain constant, even as technology changes rapidly. Unlike more research focused institutions, however, our courses emphasize the programmatic application of these fundamental concepts. Students interested in research will find an abundance of advanced topics --- medical imaging, data mining, algorithmic analysis, and scientific computing, to name a few.'
+
+INSERT INTO tblDegree (deg_name, deg_title, deg_description)
+SELECT	'Information Systems',
+		'MS In Information Systems',
+		'The MS in Information Systems fits your career path if it involves running an IT organization or managing information effectively through information systems. In today''s environment, information systems (IS) are planned, operated and maintained not only by IS departments but also by IS-user organizational units. Because of this reality, career opportunities for IS graduates are found in both traditional IS departments and IS-user business units. Moreover, a variety of equipment from aircraft to household electronics frequently use embedded systems. Graduates of IS majors find opportunities in organizations that plan, design and market the functionality of these systems. Thus, our MS-IS emphasizes the balance between technical skills, modeling proficiency, as well as strategic and organizational knowledge for a wide range of IS usage contexts.'
+
+/**
+	CONCENTRATIONS
+
+	select * from tblconcentration con
+	inner join tbldegree deg on con.deg_id = deg.deg_id
+	where deg.deg_name = 'computer science'
+*/
+
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Software and Systems Development',
+		deg.deg_id
 FROM dbo.tblDegree deg
 WHERE deg.deg_name = 'Computer Science'
 
-INSERT INTO tblConcentration (
-		con_title,
-		deg_id
-)
-SELECT
-	'Theory',
-	deg.deg_id
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Theory',
+		deg.deg_id
 FROM dbo.tblDegree deg
 WHERE deg.deg_name = 'Computer Science'
 
--- Add in rest of tblConcentration inserts.
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Data Science',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Computer Science'
 
--- Add in tblCourse inserts. or bulk insert from file.
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Database Systems',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Computer Science'
 
--- Populate tblCoursePrerequisite, one row for each prereq of a course.
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Artificial Intelligence',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Computer Science'
 
--- Populate tblDegreeRequirement, one row for each course required under degree.
--- These are the introductory/foundation courses on the degree page http://www.cdm.depaul.edu/academics/Pages/Current/Requirements-MS-in-Computer-Science.aspx
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Software Engineering',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Computer Science'
 
--- Populate tblConcentrationRequirement, one row for each course under a concentration.
--- Concentration courses can be found on the degree page http://www.cdm.depaul.edu/academics/Pages/Current/Requirements-MS-in-Computer-Science.aspx
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Multimedia',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Computer Science'
+
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Business Analysis/System Analysis',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Information Systems'
+
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Business Intelligence',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Information Systems'
+
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Database Administration',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Information Systems'
+
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'IT Enterprise',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Information Systems'
+
+INSERT INTO tblConcentration (con_title, deg_id)
+SELECT	'Standard',
+		deg.deg_id
+FROM dbo.tblDegree deg
+WHERE deg.deg_name = 'Information Systems'
+
+/**
+	COURSES
+
+	select * from tblcourse
+*/
+
+INSERT INTO tblCourse (crs_department_code, crs_number, crs_title, crs_description)
+SELECT	'CSC',
+		'401',
+		'Introduction to Programming',
+		'An introduction to programming with a focus on problem solving, structured programming, and algorithm design with a gentle introduction to efficiency. Concepts covered include data types, expressions, variables, assignments, conditional and iterative structures, functions, file input/output, exceptions, namespaces, and recursion. '
+
+INSERT INTO tblCourse (crs_department_code, crs_number, crs_title, crs_description)
+SELECT	'CSC',
+		'402',
+		'Data Structures I',
+		'A first course on data structures in Java for graduate students. The course introduces Java programming from within the context of data structures. The course covers arrays, linked lists, stacks and queues, data structures supporting disjoint-set operations, and discusses recursion and performance analysis. The implementation of the basic operations on each data structure are discussed and analyzed in terms of their efficiency.'
+
+INSERT INTO tblCourse (crs_department_code, crs_number, crs_title, crs_description)
+SELECT	'CSC',
+		'436',
+		'Web Applications',
+		'This course covers the design and development of modern web applications and their interaction with web services. The primary focus is on client-side web applications using AJAX-techniques to access web services. Possible topics include: HTML5; JavaScript/TypeScript; MVC and single-page application programming models, e.g., AngularJS; programming and security models for browsers; client-side web applications with local storage; data synchronization; HTTP, proxies, and caching; Node.js; RESTful web services.'
+
+-- Add in the rest of the course inserts. Or, bulk insert from file.
+
+/**
+	COURSE PREREQUISITES
+
+	select * from tblcourse crs
+	inner join tblcourseprerequisite prq on crs.crs_id = prq.crs_id
+	where crs.crs_department_code = 'CSC' and crs.crs_number = 402
+*/
+
+INSERT INTO tblCoursePrerequisite (crs_id, prq_id)
+SELECT	crs.crs_id, -- course id
+		(	SELECT prq.crs_id
+			FROM tblCourse prq
+			WHERE prq.crs_department_code = 'CSC'
+				AND prq.crs_number = '401') -- prereq course id
+FROM tblCourse crs
+WHERE crs.crs_department_code = 'CSC'
+	AND crs.crs_number = 402
+
+/**
+	DEGREE REQUIREMENTS
+
+	One row for each course required under degree.
+	These requirements are the introductory/foundation courses on the degree pages http://www.cdm.depaul.edu/academics/Pages/Current/Requirements-MS-in-Computer-Science.aspx
+
+	select * from tblcourse crs
+	inner join tbldegreerequirement drq on crs.crs_id = drq.crs_id
+	inner join tbldegree deg on drq.deg_id = deg.deg_id
+	where deg.deg_name = 'computer science'
+*/
+
+INSERT INTO tblDegreeRequirement(deg_id, crs_id)
+SELECT	deg.deg_id, -- degree id
+		(	SELECT prq.crs_id
+			FROM tblCourse prq
+			WHERE prq.crs_department_code = 'CSC'
+				AND prq.crs_number = '401') -- prereq course id
+FROM tblDegree deg
+WHERE deg.deg_name = 'Computer Science'
+
+INSERT INTO tblDegreeRequirement(deg_id, crs_id)
+SELECT	deg.deg_id, -- degree id
+		(	SELECT prq.crs_id
+			FROM tblCourse prq
+			WHERE prq.crs_department_code = 'CSC'
+				AND prq.crs_number = '402') -- prereq course id
+FROM tblDegree deg
+WHERE deg.deg_name = 'Computer Science'
+
+/**
+	CONCENTRATION COURSES
+
+	One row for each course under a concentration.
+	Concentration courses can be found on the degree pages http://www.cdm.depaul.edu/academics/Pages/Current/Requirements-MS-in-Computer-Science.aspx
+
+	select * from tblcourse crs
+	inner join tblconcentrationcourse cos on crs.crs_id = cos.crs_id
+	inner join tblconcentration con on cos.con_id = con.con_id
+	where con.con_title = 'software and systems development'
+*/
+
+INSERT INTO tblConcentrationCourse(con_id, crs_id)
+SELECT	con.con_id, -- concentration id
+		(	SELECT prq.crs_id
+			FROM tblCourse prq
+			WHERE prq.crs_department_code = 'CSC'
+				AND prq.crs_number = '436') -- prereq course id
+FROM tblConcentration con
+WHERE con.con_title = 'Software and Systems Development'
+
+/**
+	PREDEFINED ACCOUNTS
+
+	select * from tblaccount
+*/
 
 INSERT INTO tblAccount (
 	acc_username,
@@ -70,6 +215,7 @@ SELECT
 	'John',
 	'Doe',
 	'9000 W Western Ave',
+	'Chicago',
 	'IL',
 	'60601',
 	'3125550099',
@@ -77,6 +223,75 @@ SELECT
 	deg.deg_id,
 	con.con_id
 FROM dbo.tblDegree deg
-	INNER JOIN dbo.tblConcentration con ON deg.deg_id = con.con_id
-WHERE deg.deg_name = 'Computer Science'
-AND con.con_title = 
+	INNER JOIN dbo.tblConcentration con ON deg.deg_id = con.deg_id
+WHERE con.con_title = 'Theory'
+
+/**
+	CLASSES OFFERED
+
+	clo_quarter CHAR(20) NOT NULL, -- FALL, WINTER, SPRING, SUMMERI, SUMMERII, SUMMER10WEEK, DECINTERSESSION
+	clo_day_of_week CHAR(5) NOT NULL, -- O = Online, M = Monday, TU = Tuesday, W = Wednesday, TH = Thursday, F = Friday, MW = Mon/Wed, MWF = Mon/Wed/Fri, TUTH = Tues/Thurs
+	clo_location CHAR(15) DEFAULT 'ONLINE', -- ONLINE, LOOP, LINCOLNPARK, etc.
+
+	select * from tblclassoffering clo
+	inner join tblcourse crs on clo.crs_id = crs.crs_id
+*/
+
+INSERT INTO tblClassOffering (crs_id, clo_year, clo_quarter, clo_day_of_week, clo_location)
+SELECT	crs.crs_id,
+		2016,
+		'WINTER',
+		'O',
+		'ONLINE'
+FROM tblCourse crs
+WHERE crs.crs_department_code = 'CSC'
+	AND crs.crs_number = 401
+
+INSERT INTO tblClassOffering (crs_id, clo_year, clo_quarter, clo_day_of_week, clo_location)
+SELECT	crs.crs_id,
+		2016,
+		'FALL',
+		'M',
+		'LOOP'
+FROM tblCourse crs
+WHERE crs.crs_department_code = 'CSC'
+	AND crs.crs_number = 402
+
+INSERT INTO tblClassOffering (crs_id, clo_year, clo_quarter, clo_day_of_week, clo_location)
+SELECT	crs.crs_id,
+		2016,
+		'WINTER',
+		'M',
+		'LOOP'
+FROM tblCourse crs
+WHERE crs.crs_department_code = 'CSC'
+	AND crs.crs_number = 402
+
+INSERT INTO tblClassOffering (crs_id, clo_year, clo_quarter, clo_day_of_week, clo_location)
+SELECT	crs.crs_id,
+		2016,
+		'WINTER',
+		'O',
+		'ONLINE'
+FROM tblCourse crs
+WHERE crs.crs_department_code = 'CSC'
+	AND crs.crs_number = 402
+
+/**
+	PREDEFINED CLASS HISTORY
+
+	select * from tblclasshistory clh
+	inner join tblaccount acc on clh.acc_id = acc.acc_id
+	where acc.acc_username = 'admin@depaul.edu'
+*/
+
+INSERT INTO tblClassHistory (acc_id, clo_id, clh_grade)
+SELECT	acc.acc_id,
+		(	SELECT MAX(clo.clo_id)
+			FROM tblClassOffering clo
+				INNER JOIN tblCourse crs ON clo.crs_id = crs.crs_id
+			WHERE crs.crs_department_code = 'CSC'
+				AND crs.crs_number = 401),
+		'A'
+FROM tblAccount acc
+WHERE acc.acc_username = 'admin@depaul.edu'
