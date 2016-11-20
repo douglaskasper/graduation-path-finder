5d643afb08ac817ea290641c8b4396021717f782
Douglas Kasper <dkasper879@gmail.com>
2016-11-20 09:34:51 -0600

    Account role can be updated from UI.

--- GPF.Web/ViewModels/AccountViewModel.cs
+++ GPF.Web/ViewModels/AccountViewModel.cs
@@ -8,6 +8,7 @@ namespace GPF.Web.ViewModels
         public Account Account { get; set; }
         public List<Degree> AvailableDegrees { get; set; }
         public List<Account> StudentAccounts { get; set; }
+        public List<Account> ElevatedAccounts { get; set; }
         public bool Impersonating { get; set; }
         public List<GPFSession> SavedSessions { get; set; }
         public List<Course> CourseHistory { get; set; }
