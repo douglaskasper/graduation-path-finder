d3bc157f959cf719862fbd44463b4692ab50a09f
WCotterman <wcotterman@gmail.com>
2016-11-21 14:35:50 -0600

    Remove Duplicates
    Added a second prerequisite search pass to reduce the number of classes
    returned
    Manually remove duplicate classes at the end

--- GPF.Domain/Services/GPFService.cs
+++ GPF.Domain/Services/GPFService.cs
@@ -164,6 +164,7 @@ namespace GPF.Domain.Services
         {
             List<int> toTake = new List<int>();
             List<int> missing = new List<int>();
+            List<Course> temp;
             decimal hours = 0;
             //add major courses based on session
             CourseService crsServ = new CourseService();
@@ -180,36 +181,6 @@ namespace GPF.Domain.Services
             {
                 courses.Add(crsServ.GetCoursesById(course.Id));
                 toTake.Add(course.Id);
-            }
-
-            //determine number of hours being taken
-            foreach (Course course in courses)
-            {
-                hours += course.Units;
-            }
-            //if less than 48 are being taken, add new concentration classes until at least 48 hours worth of classes are there
-            if (hours < 48)
-            {
-                int count = 0;
-                List<Course> extras = crsServ.GetCoursesByConcentration(session.Concentration.Id);
-                foreach(Course course in extras)
-                {
-                    course.Prerequisites = crsServ.GetCoursePrereqs(course.Id);
-                    if (course.Prerequisites == null)
-                    {
-                        course.Prerequisites = new List<Course>();
-                    }
-                }
-                while (hours < 48)
-                {
-                    if (!extras[count].isContainedBy(courses) && extras[count].Prerequisites.Count == 0)
-                    {
-                        courses.Add(extras[count]);
-                        toTake.Add(extras[count].Id);
-                        hours += extras[count].Units;
-                    }
-                    count++;
-                }
             }
             foreach (Course course in courses)
             {
@@ -255,7 +226,88 @@ namespace GPF.Domain.Services
                     }
                 }
             }
-
+            //remove any duplicates
+            temp = new List<Course>();
+            foreach (Course course in courses)
+            {
+                if (!course.isContainedBy(temp)) temp.Add(course);
+            }
+            courses.Clear();
+            courses.AddRange(temp);
+            //determine number of hours being taken
+            foreach (Course course in courses)
+            {
+                hours += course.Units;
+            }
+            //if less than 48 are being taken, add new concentration classes until at least 48 hours worth of classes are there
+            if (hours < 48)
+            {
+                int count = 0;
+                List<Course> extras = crsServ.GetCoursesByConcentration(session.Concentration.Id);
+                foreach(Course course in extras)
+                {
+                    course.Prerequisites = crsServ.GetCoursePrereqs(course.Id);
+                    if (course.Prerequisites == null)
+                    {
+                        course.Prerequisites = new List<Course>();
+                    }
+                }
+                while (hours < 48)
+                {
+                    if (!extras[count].isContainedBy(courses) && extras[count].Prerequisites.Count == 0)
+                    {
+                        courses.Add(extras[count]);
+                        toTake.Add(extras[count].Id);
+                        hours += extras[count].Units;
+                    }
+                    count++;
+                }
+            }
+            
+            //add any missing prereqs
+            foreach (Course course in courses)
+            {
+                foreach (Course pre in course.Prerequisites)
+                {
+                    if (!toTake.Contains(pre.Id))
+                    {
+                        missing.Add(pre.Id);
+                    }
+                }
+            }
+            while (missing.Count > 0)
+            {
+                foreach (int misId in missing.ToArray())
+                {
+                    Course toAdd = crsServ.GetCoursesById(misId);
+                    toAdd.Prerequisites = crsServ.GetCoursePrereqs(misId);
+                    if (toAdd.Prerequisites == null)
+                    {
+                        toAdd.Prerequisites = new List<Course>();
+                    }
+                    courses.Add(toAdd);
+                    toTake.Add(misId);
+                    missing.Remove(misId);
+                }
+                foreach (Course course in courses)
+                {
+                    foreach (Course pre in course.Prerequisites)
+                    {
+                        if (!toTake.Contains(pre.Id))
+                        {
+                            missing.Add(pre.Id);
+                        }
+                    }
+                }
+            }
+            //remove any duplicates
+            temp = new List<Course>();
+            foreach (Course course in courses)
+            {
+                if (!course.isContainedBy(temp)) temp.Add(course);
+            }
+            courses.Clear();
+            courses.AddRange(temp);
             //sort courses using tree
             CourseTree tree = new CourseTree(courses);
             List<Course> sortedCourses = tree.GetList();
